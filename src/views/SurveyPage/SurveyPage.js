938200b7b35608da41a8fa9898bfc74569ea4625
ramshanadeem <rimshanadeem64@gmail.com>
2022-01-05 02:19:32 +0500

    low routes

--- src/views/SurveyPage/SurveyPage.js
+++ src/views/SurveyPage/SurveyPage.js
@@ -1,121 +1,6 @@
 /* eslint-disable prettier/prettier */
 // /* eslint-disable prettier/prettier */
-// import React from "react";
-// import Low from "../../assets/img/Low.jpg";
-// import High from "../../assets/img/High.jpg";
-// // import Button from "components/CustomButtons/Button.js";
-// import Medium from "../../assets/img/Medium.png";
-// // import { useNavigate } from "react-router-dom";
-// import Button from "@mui/material/Button";
-// import { useNavigate } from "react-router-dom";
 
-// export default function SurveyPage() {
-//   const navigate = useNavigate();
-
-//   const screening = () => {
-//     navigate("/screeningForm");
-//   };
-//   return (
-//     <div style={{ backgroundColor: "#fff" }}>
-//       <h1
-//         style={{
-//           color: "black",
-//           display: " inline-block",
-//           position: "relative",
-//           fontSize: "4.2rem",
-//           fontWeight: 600,
-//           width: "100%",
-//           textAlign: "center",
-//         }}
-//       >
-//         Well-Being
-//       </h1>
-//       <div
-//         style={{
-//           textAlign: "center",
-//           flexDirection: "column",
-//           display: "flex",
-//           marginTop: "6%",
-//         }}
-//       >
-//         <div
-//           style={{
-//             display: "flex",
-//             flexDirection: "row",
-//             justifyContent: "space-around",
-//           }}
-//         >
-//           <Button
-//             onClick={screening}
-//             color="rose"
-//             style={{
-//               width: "26%",
-//               marginLeft: "40px",
-//               height: "48px",
-//               marginTop: "26px",
-//             }}
-//             round
-//           >
-//             Low
-//           </Button>
-//           <div>
-//             <img style={{ width: "43%", height: "69%" }} src={Low} alt="" />
-//           </div>
-//         </div>
-//         <div
-//           style={{
-//             display: "flex",
-//             flexDirection: "row",
-//             marginLeft: "3%",
-//           }}
-//         >
-//           <Button
-//             // onClick={survey}
-//             color="warning"
-//             style={{
-//               width: "26%",
-//               //   marginLeft: "40px",
-//               marginRight: "200px",
-
-//               height: "48px",
-//               marginTop: "26px",
-//             }}
-//             round
-//           >
-//             Medium
-//           </Button>
-//           <div>
-//             <img style={{ width: "92%", height: "67%" }} src={Medium} alt="" />
-//           </div>
-//         </div>
-//         <div
-//           style={{
-//             display: "flex",
-//             flexDirection: "row",
-//             justifyContent: "space-around",
-//           }}
-//         >
-//           <Button
-//             color="success"
-//             // onClick={survey}
-//             style={{
-//               width: "26%",
-//               marginLeft: "40px",
-//               height: "48px",
-//               marginTop: "26px",
-//             }}
-//             round
-//           >
-//             High
-//           </Button>
-//           <div>
-//             <img style={{ width: "43%", height: "69%" }} src={High} alt="" />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 import React from "react";
 // nodejs library that concatenates classes
 // import classNames from "classnames";
@@ -127,11 +12,11 @@ import High from "../../assets/img/High.jpg";
 // import Button from "components/CustomButtons/Button.js";
 import Medium from "../../assets/img/Medium.png";
 // import { useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 // @material-ui/icons
 // core components
 // sections for this page
-
+import Button from "components/CustomButtons/Button.js";
 // import SectionBasics from "./Sections/SectionBasics.js";
 // import SectionNavbars from "./Sections/SectionNavbars.js";
 // import SectionTabs from "./Sections/SectionTabs.js";
@@ -187,6 +72,7 @@ export default function SurveyPage() {
           >
             <Button
               onClick={survey}
+              color="rose"
               style={{
                 width: "26%",
                 marginLeft: "40px",
@@ -210,12 +96,12 @@ export default function SurveyPage() {
           >
             <Button
               // onClick={survey}
-
+              color="warning"
               style={{
                 width: "26%",
                 //   marginLeft: "40px",
                 marginRight: "200px",
-
+                backgroundColor: "Blue",
                 height: "48px",
                 marginTop: "26px",
               }}
@@ -240,6 +126,7 @@ export default function SurveyPage() {
           >
             <Button
               // onClick={survey}
+              color="success"
               style={{
                 width: "26%",
                 marginLeft: "40px",
